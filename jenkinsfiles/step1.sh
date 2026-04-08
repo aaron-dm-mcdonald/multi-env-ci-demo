a1805fa76f86d779988014f717d9d6efdb73ed24
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2026-04-07 17:33:06 -0700

    add ssm

--- jenkinsfiles/step1.sh
+++ jenkinsfiles/step1.sh
@@ -4,15 +4,15 @@ VERSION=$2
 BUCKET=$3
 
 INSTANCE_ID=$(aws ec2 describe-instances \
-  --filters "Name=tag:Name,Values=dev-app" "Name=instance-state-name,Values=running" \
+  --filters "Name=tag:env,Values=$ENV" "Name=instance-state-name,Values=running" \
   --query 'Reservations[0].Instances[0].InstanceId' \
   --output text)
 
-aws ec2-instance-connect ssh \
-  --instance-id "$INSTANCE_ID" \
-  --os-user ec2-user \
-  --command "
-    aws s3 cp s3://$BUCKET/app/$VERSION/app /home/ec2-user/app/app &&
-    chmod +x /home/ec2-user/app/app &&
-    sudo systemctl restart go-api
-  "
+aws ssm send-command \
+  --instance-ids "$INSTANCE_ID" \
+  --document-name "AWS-RunShellScript" \
+  --parameters "commands=[
+    'aws s3 cp s3://$BUCKET/app/$VERSION/app /home/ec2-user/app/app',
+    'chmod +x /home/ec2-user/app/app',
+    'sudo systemctl restart go-api'
+  ]"
